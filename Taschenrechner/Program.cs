f8978a7839f3e6689518e3450626203de6f00198
Niko Jünger <niko.juenger@gmx.de>
2018-09-11 11:15:09 +0200

    Start

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, result;
+            a = 1;
+            b = 5;
+            result = Addition(a, b);
+                
+        }
+        static int Addition(int a, int b)
+        {
+            int result;
+            result = a + b;
+            return result;
+        }
+    }
+    
+}
